e9a313a43ac4281917c2d2cc6f6f407add41f788
Sujan Thakare <sujan.thakare@atg.se>
2022-06-29 19:37:57 +0200

    Added placeholder pages

---
+++ src/pages/home.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Box, Container, Typography } from '@mui/material';
+
+const Home = () => {
+  return (
+    <Container>
+      <Box>
+        <Typography>HOME</Typography>
+      </Box>
+    </Container>
+  );
+};
+
+export default Home;
---
+++ src/pages/watch-later.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WatchLater = () => {
+  return <h1>WATCH LATER</h1>;
+};
+
+export default WatchLater;
