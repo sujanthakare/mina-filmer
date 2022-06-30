4bffb091521d5ed64a7e3d6049513a4de35bb592
Sujan Thakare <sujan.thakare@atg.se>
2022-06-30 19:49:38 +0200

    implemented generic movie grid

---
+++ src/widgets/movie-grid.tsx
@@ -0,0 +1,64 @@
+import { Grid } from '@mui/material';
+
+import useFavorites from '../data/hooks/useFavorites';
+import useWatchList from '../data/hooks/useWatchList';
+import { IMovie } from '../data/types';
+import MovieCard from '../ui/movie-card';
+
+type Props = {
+  movies: Array<IMovie>;
+};
+
+const MovieGrid = ({ movies }: Props) => {
+  const { addFavorite, removeFavorite, favorites } = useFavorites();
+  const { addToWatchList, removeFromWatchList, watchList } = useWatchList();
+
+  return (
+    <>
+      {movies && (
+        <Grid container spacing={1}>
+          {movies?.map((item) => {
+            if (!item.poster_path) {
+              return null;
+            }
+
+            const isFavorite = favorites.some(
+              (favorite) => favorite.id === item.id
+            );
+
+            const isInWatchList = watchList.some(
+              (watchListItem) => watchListItem.id === item.id
+            );
+
+            return (
+              <Grid key={item.id} item xs={6} md={4} xl={3}>
+                <MovieCard
+                  movie={{
+                    title: item.title,
+                    poster_path: item.poster_path,
+                  }}
+                  favorite={isFavorite}
+                  watchLater={isInWatchList}
+                  onFavoriteClick={() => {
+                    if (isFavorite) {
+                      return removeFavorite(item);
+                    }
+                    addFavorite(item);
+                  }}
+                  onWatchLaterClick={() => {
+                    if (isInWatchList) {
+                      return removeFromWatchList(item);
+                    }
+                    addToWatchList(item);
+                  }}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
+    </>
+  );
+};
+
+export default MovieGrid;
