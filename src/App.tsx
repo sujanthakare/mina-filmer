c7333a914f1d8bc97aca3020e787b837cf788e37
Sujan Thakare <sujan.thakare@atg.se>
2022-06-30 19:44:29 +0200

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
+import { Container } from '@mui/material';
 
 import Favorites from './pages/favorites';
 import Home from './pages/home';
 import WatchLater from './pages/watch-later';
+import Header from './ui/header';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/watch-later" element={<WatchLater />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Container maxWidth="xl">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/watch-later" element={<WatchLater />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Container>
   );
 }
 
