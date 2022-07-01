9ec78b6363ba24837bf7940a2a547958e517f157
Sujan Thakare <sujan.thakare@atg.se>
2022-07-01 17:35:00 +0200

    added tests for useSearchQuery hook

--- src/data/hooks/useSearchQuery.test.tsx
+++ src/data/hooks/useSearchQuery.test.tsx
@@ -1,11 +1,13 @@
 import { Provider } from 'react-redux';
 
-import { Store, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { act, renderHook } from '@testing-library/react-hooks';
 
 import useSearchQuery from './useSearchQuery';
 
-const render_useSearchQuery = (mockStore: Store) => {
+const render_useSearchQuery = () => {
+  const mockStore = configureStore({ reducer: {} });
+
   const wrapper = ({ children }: any) => (
     <Provider store={mockStore}>{children}</Provider>
   );
@@ -16,21 +18,15 @@ const render_useSearchQuery = (mockStore: Store) => {
 };
 
 describe('useSearchQuery', () => {
-  let mockStore: Store;
-
-  beforeEach(() => {
-    mockStore = configureStore({ reducer: {} });
-  });
-
   it('should return default query text and page', () => {
-    const { result } = render_useSearchQuery(mockStore);
+    const { result } = render_useSearchQuery();
 
     expect(result.current.queryPage).toBe(1);
     expect(result.current.queryText).toBe('');
   });
 
   it('should update query page', () => {
-    const { result } = render_useSearchQuery(mockStore);
+    const { result } = render_useSearchQuery();
 
     act(() => {
       result.current.setQueryPage(2);
@@ -41,7 +37,7 @@ describe('useSearchQuery', () => {
   });
 
   it('should update query text', () => {
-    const { result } = render_useSearchQuery(mockStore);
+    const { result } = render_useSearchQuery();
 
     const queryText = 'search query';
     act(() => {
