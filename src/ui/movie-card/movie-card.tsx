560fe763bd2fd81b51e1b82e71e2f9058be1782b
Sujan Thakare <sujandt@gmail.com>
2022-07-01 23:46:21 +0200

    movie card new design

--- src/ui/movie-card/movie-card.tsx
+++ src/ui/movie-card/movie-card.tsx
@@ -1,6 +1,6 @@
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import WatchLaterIcon from '@mui/icons-material/WatchLater';
-import { Card, CardActions, CardMedia, IconButton } from '@mui/material';
+import { Box, Card, CardMedia, IconButton } from '@mui/material';
 
 type Props = {
   movie: { title: string; poster_path: string; id: number };
@@ -18,28 +18,45 @@ const MovieCard = ({
   watchLater,
 }: Props) => {
   return (
-    <Card data-testId="movie-card">
+    <Card
+      data-testid="movie-card"
+      sx={{ height: '100%', position: 'relative' }}
+    >
       <CardMedia
         component="img"
+        sx={{ height: 'inherit', aspectRatio: '2/3' }}
         image={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
         alt={movie.title}
       />
-      <CardActions>
+
+      <Box
+        className="card-actions"
+        sx={{
+          position: 'absolute',
+          bottom: 0,
+          right: 0,
+          margin: 1,
+          height: 36,
+          padding: '0px 4px 0px 4px',
+          borderRadius: 10,
+          backgroundColor: 'rgba(255, 255, 255, 0.8)',
+        }}
+      >
         <IconButton
-          data-testId={`favorite-button-${movie.id}`}
+          data-testid={`favorite-button-${movie.id}`}
           onClick={onFavoriteClick}
           aria-label="Mark favorite"
         >
           <FavoriteIcon color={favorite ? 'error' : 'inherit'} />
         </IconButton>
         <IconButton
-          data-testId={`watch-later-button-${movie.id}`}
+          data-testid={`watch-later-button-${movie.id}`}
           aria-label="Mark watch later"
           onClick={onWatchLaterClick}
         >
           <WatchLaterIcon color={watchLater ? 'info' : 'inherit'} />
         </IconButton>
-      </CardActions>
+      </Box>
     </Card>
   );
 };
